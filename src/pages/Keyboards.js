4864e934af0b1f5e70852d9ee2cb1a8c37c8193b
wongzl <zhenglong.wong@gmail.com>
2022-06-01 17:43:29 +0800

    completed  mvp for all catalogue pages and respective tests

--- src/pages/Keyboards.js
+++ src/pages/Keyboards.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Catalogue from '../components/Catalogue';
 const Keyboards = () => {
 	return (
 		<>
 			<Navbar />
 			<h1>Keyboards</h1>
+			<Catalogue type="Keyboard" />
 		</>
 	);
 };
