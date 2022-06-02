c70bce403225e7b38dcf00923460b39dae942f2e
wongzl <zhenglong.wong@gmail.com>
2022-06-02 17:14:13 +0800

    add cart context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,6 @@ import { Link } from "react-router-dom";
 
 function HideOnScroll(props) {
 	const { children, window } = props;
-	// Note that you normally won't need to set the window ref as useScrollTrigger
-	// will default to window.
-	// This is only being set here because the demo is in an iframe.
 	const trigger = useScrollTrigger({
 		target: window ? window() : undefined,
 	});
